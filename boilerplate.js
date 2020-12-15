582e99216d37949f277672c7c66b2e9f3dd3ead7
Ray Bejjani <ray.bejjani@gmail.com>
2020-12-16 00:33:01 +0400

    Basic lunar lander w/fuel and crash
    Signed-off-by: Ray Bejjani <ray.bejjani@gmail.com>

---
+++ boilerplate.js
@@ -0,0 +1,114 @@
+/*
+
+ - Boilerplate code for use in recreating Asteroids -
+
+
+
+What this gives us:
+
+
+bpMakeCanvas(width, height)
+ Creates a canvas object for drawing, and puts it into the DOM
+
+bpDrawCircle(x, y, radius, color)
+ Draws a circle on the canvas
+
+bpClearCanvas()
+ Erases everything that's been drawn on the canvas
+
+bpStartGameLoop(loopFunction)
+ Calls the passed function ~60 times a second (or whatever monitor refresh rate is)
+
+Variables representing the state of our arrow keys:
+	ksup   
+	ksdown 
+	ksleft 
+	ksright
+
+*/
+
+
+
+// Making these global, half for readability and half because I'm lazy.
+//  This is bad practice obviously; don't do this in your own code!
+var canv;
+var ksup    = false;
+var ksdown  = false;
+var ksleft  = false;
+var ksright = false;
+
+// Toggle to print keystrokes
+var log_keys = false;
+
+// Pause
+var run = true;
+
+// Create canvas element for drawing
+function bpMakeCanvas(width, height)
+{
+	canv = document.createElement("canvas");
+	canv.width = width;
+	canv.height = height;
+	canv.style.background = "black";
+	window.addEventListener("load", function()
+	{
+		document.body.append(canv);
+	});
+
+	return canv;
+}
+
+
+// Clears the canvas
+function bpClearCanvas()
+{
+	var ctx = canv.getContext("2d");
+	ctx.clearRect(0, 0, canv.width, canv.height);
+}
+
+
+// Draws a filled circle (adjusting y so it's bottom-up)
+function bpDrawCircle(x, y, radius, color)
+{
+	var ctx = canv.getContext("2d");
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.arc(x, canv.height-y, radius, 0, 2*Math.PI);
+	ctx.fill();
+}
+
+
+// Add Keyboard up/down event listeners, and have them modify our keystate variables
+window.addEventListener("keydown",function(e)
+{
+	if(log_keys) console.log("down " + e.code);
+	if(e.code == "ArrowUp")    ksup    = true;
+	if(e.code == "ArrowDown")  ksdown  = true;
+	if(e.code == "ArrowLeft")  ksleft  = true;
+	if(e.code == "ArrowRight") ksright = true;
+});
+window.addEventListener("keyup",function(e)
+{
+	if(log_keys) console.log("up " + e.code);
+	if(e.code == "ArrowUp")    ksup    = false;
+	if(e.code == "ArrowDown")  ksdown  = false;
+	if(e.code == "ArrowLeft")  ksleft  = false;
+	if(e.code == "ArrowRight") ksright = false;
+});
+
+
+// Run game loop function at monitor's refresh rate
+function bpStartGameLoop(loopFunction)
+{
+	// To have this run independently of users framerate, see this article and implement
+	//  his technique:  https://www.gafferongames.com/post/fix_your_timestep/
+
+	function repeat()
+	{
+		if (run) loopFunction();
+		window.requestAnimationFrame(repeat);
+	}
+	window.requestAnimationFrame(repeat);
+}
+
+
--- main.js
+++ main.js
@@ -1,97 +1,135 @@
-var canv = document.createElement("canvas");
-canv.width=500;
-canv.height=700;
-canv.style.backgroundColor = "black";
-window.addEventListener("load", function() {
-	document.body.append(canv)
-});
+/**** Config ****/
+// Screen dimensions
+var width = 600;
+var height = 400;
 
-var ctx = canv.getContext("2d");
+// General game state
+var terrain_height = 20;
+var starting_fuel = 100;
+var crash_speed = 0.8;
 
-/**
-ctx.fillstyle = "red";
-ctx.fillRect(100, 100, 30, 30);
-**/
-
-var run = true;
-var log_keys = false;
+// Physics
+var gravity = -0.01;
 var drag_coefficient = 0.99;
 var thrust_strength = 0.2;
-var gravity = 0.05;
+var turn_strength = 0.05; // radians 
 
-var x = 100;
-var y = 100;
+// Ship properties
+var r = 10;  // radius of ship's big main circle
+var rs = 4;  // radius of ship's front small circle
+var ds = 15; // distance between the circles
+var fuel = starting_fuel;
 
-var x_d = 0;
-var y_d = 0;
+// Ship state
+var x = 200; // ship x coordinate
+var y = 100; // ship y coordinate
+var a = 0.5*Math.PI;   // ship angle
 
-var up_thrust = false;
-var right_thrust = false;
-var left_thrust = false;
+var dx = 0; // ship x speed
+var dy = 0; // ship y speed
+var da = 0;   // ship anglular velocity
 
+
+function drawShip(x, y, a) {
+	// Draw the big circle
+	bpDrawCircle(x, y, r, "red");
+
+	// Draw the small circle, offset by the ship's angle
+	var xs = x + ds*Math.cos(a)
+	var ys = y + ds*Math.sin(a)
+	bpDrawCircle(xs, ys, rs, "red");
+}
+
+
+function drawTerrain(y) {
+	var ctx = canv.getContext("2d");
+	ctx.fillStyle = "beige";
+  ctx.fillRect(0, canv.height-y, canv.width, y);
+}
+
+function drawLose() {
+	var ctx = canv.getContext("2d");
+	ctx.fillStyle = "red";
+	ctx.font = '16px serif';
+	ctx.fillText("Crash!\r\nSuper Sad Spaceship :(", canv.width/4, canv.height/2);
+}
+
+function drawUI() {
+	var ctx = canv.getContext("2d");
+	ctx.fillStyle = "white";
+	ctx.font = '16px serif';
+
+	// Altitude from the bottom of the big circle to the ground
+	var altitude = (y - r - terrain_height).toFixed(2);
+	ctx.fillText("Altitude: " + altitude, 10, 16);
+
+	// Fuel
+	ctx.fillStyle = (fuel <= 0) ? "red" : "white";
+	ctx.fillText("Fuel: " + fuel.toFixed(2), canv.width - 85, 16);
+
+	// Speed indicator, in red when crashing is possible
+	speed = Math.sqrt(dx**2 + dy**2);
+	ctx.fillStyle = (speed > crash_speed && dy < 0) ? "red" : "white";
+	ctx.fillText("Speed: " + speed.toFixed(2), 10, 16+16); //+16 from 16px altitude text above
+}
+
+/**** Init ****/
+// Canvas setup
+var canv = bpMakeCanvas(width, height);
+var ctx = canv.getContext("2d");
+
+// Extra handler for pause via "Space"
 window.addEventListener("keydown", function(e) {
 	if(log_keys) console.log("down " + e.code);
+
 	switch(e.code) {
-		case "ArrowRight":
-			right_thrust = true;
-			break;
-		case "ArrowLeft":
-			left_thrust = true;
-			break;
-		case "ArrowUp":
-			up_thrust = true;
-			break;
-		case "ArrowDown":
-			break;
 		case "Space":
 			if(!run) window.requestAnimationFrame(loop);
 			run = !run;
 	}
 });
 
-window.addEventListener("keyup", function(e) {
-	if(log_keys) console.log("up " + e.code);
-
-	switch(e.code) {
-		case "ArrowRight":
-			right_thrust = false;
-			break;
-		case "ArrowLeft":
-			left_thrust = false;
-			break;
-		case "ArrowUp":
-			up_thrust = false;
-			break;
-		case "ArrowDown":
-			break;
-	}});
-
-
 function loop() {
-	ctx.clearRect(0,0,canv.width,canv.height);
+	/**** Physics updates ****/
+	// Change the angle based on left/right arrow keys
+	if(ksleft)  a+= turn_strength;
+	if(ksright) a-= turn_strength;
 
-	if(up_thrust)    y_d += -thrust_strength;
-	                 y_d += +gravity; // gravity
-	if(right_thrust) x_d += +thrust_strength;
-	if(left_thrust)  x_d += -thrust_strength;
-
-	x+=x_d;
-	x_d*=drag_coefficient;
-	x = x % canv.width;
-	if(x<0) x+=canv.width;
-
-	if(y>=canv.height) y_d*=-1;
-	y+=y_d;
-	y_d*=drag_coefficient;
-	//y = y % canv.height;
-	//if(y<0) y+=canv.height;
-
-	ctx.fillStyle = "red"
-	ctx.fillRect(x,y,30,30);
-
-	if(run) {
-		window.requestAnimationFrame(loop);
+	// Move the ship forward, based on it's angle
+	if(ksup && fuel > 0)
+	{
+		dx+= thrust_strength*Math.cos(a);
+		dy+= thrust_strength*Math.sin(a);
+		fuel-= 1;
 	}
+	dx*= drag_coefficient;
+	dy*= drag_coefficient;
+	dy+= gravity; // gravity
+
+	x+= dx;
+	y+= dy;
+
+  // Ground collision
+  var need_drawLose = false;
+	if(y-r <= terrain_height){
+		speed = Math.sqrt(dx**2 + dy**2);
+		if(speed > crash_speed) {
+			run = false;
+			need_drawLose = true;
+		} else {
+			y = r + terrain_height; 
+			dy = 0;
+		}
+	}
+
+	/**** Drawing ****/
+	// Clear the canvas
+	bpClearCanvas();
+
+	drawUI();
+	drawShip(x, y, a);
+	drawTerrain(terrain_height);
+	if(need_drawLose) drawLose();
 }
 
-window.requestAnimationFrame(loop);
+bpStartGameLoop(loop);
